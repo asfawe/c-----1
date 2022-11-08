5b6c0f0d0d6402e3dc75067033a3c37993c823c3
unknown <aronia5137@gmail.com>
2022-11-09 08:39:54 +0900

    first commit

---
+++ p2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b,c;
+	int i;
+	scanf("%d", &a);
+	for(i = 0; i < a; i++)
+	{
+		scanf("%d %d", &b,&c);
+		printf("%d \n", b + c);
+	}
+	
+	
+	return 0;
+}
